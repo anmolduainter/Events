12dc7d2c0bbee5903596675f51746e6a84ae8cc9
anmolduainter <anmolduainter@gmail.com>
2017-08-05 07:52:45 +0530

    Js

--- public_static/js/RegisteredEvents.js
+++ public_static/js/RegisteredEvents.js
@@ -24,6 +24,59 @@ function notLike(eve){
 
 
 
+    console.log("Hello");
+
+    let id=$(this).parent().attr('id');
+
+    console.log(id);
+
+    let name=$(this).parent().find('h1').text();
+    let date=$(this).parent().find('h2').text().substr(0,10);
+    let time=$(this).parent().find('h2').text().replace(date+" ",'');
+    let Ptime;
+    if (time.length==19){
+        Ptime=$(this).parent().find('h2').text().replace(date+" ",'').substr(1,17);
+    }
+    else if(time.length==20){
+        Ptime=$(this).parent().find('h2').text().replace(date+" ",'').substr(1,18);
+    }
+    else if(time.length==21){
+        Ptime=$(this).parent().find('h2').text().replace(date+" ",'').substr(1,19);
+    }
+    console.log("Name : "+name);
+    console.log("Date : "+date);
+    console.log("Time : "+time);
+    console.log("ParticularTime : "+Ptime);
+
+
+    let query={
+        name:name,
+        date:date,
+        time:Ptime
+    };
+
+    $.post('/Events/NotLikeEvents',query,(data)=>{
+
+
+        if (data.success){
+
+            $.post('/Events/RegisteredEvents',query,(data1)=>{
+
+                if (data1.success){
+                    window.location.reload(true);
+                }
+
+            })
+
+        }
+
+        else{
+            alert("Try Again Later");
+        }
+
+    })
+
+
 }
 
 function like(eve){
@@ -65,7 +118,15 @@ function like(eve){
 
 
         if (data.success){
-            window.location.reload(true);
+
+            $.post('/Events/RegisteredEvents',query,(data1)=>{
+
+                if (data1.success){
+                    window.location.reload(true);
+                }
+
+            })
+
         }
 
         else{
