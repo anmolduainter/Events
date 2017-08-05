096c6bbd5a82e64b2c53d938d2d33fb222d9c40b
anmolduainter <anmolduainter@gmail.com>
2017-08-05 07:45:37 +0530

    Not Like Events

--- routers/Events/NotLikeEvents.js
+++ routers/Events/NotLikeEvents.js
@@ -1,3 +1,72 @@
 /**
  * Created by anmol on 3/8/17.
  */
+
+const router=require('express').Router();
+const sqldb=require('../../DataBase/sqlDatabase.js');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId=require('mongodb').ObjectId;
+
+let url = 'mongodb://localhost:27017/myproject';
+
+
+router.post('/',(req,res)=>{
+
+
+    MongoClient.connect(url,(err,db)=>{
+
+        let name=req.body.name.toString();
+        let date=req.body.date.toString();
+        let time=req.body.time.toString();
+
+        console.log(name);
+        console.log(date);
+        console.log(time);
+
+
+        db.collection('AllEvents').find({name: name,date: date, time: time}).toArray(function (err,result) {
+
+            let query={
+                events_id:result[0]._id.toString()
+            };
+
+
+            sqldb.leader.findOne({where:query}).then(function (result) {
+
+
+                if (result==undefined){
+
+                }
+
+                else{
+
+                    let dislike=+(result.disLikes)+1;
+
+                    console.log(result.disLikes);
+
+                    let id=result.events_id;
+
+                    sqldb.leader.update({disLikes:dislike},{where:{events_id:id}}).then(function () {
+
+                        res.send({success:true});
+
+                    });
+
+
+                }
+
+            })
+
+
+
+        })
+
+
+    })
+
+});
+
+
+
+
+module.exports=router;
